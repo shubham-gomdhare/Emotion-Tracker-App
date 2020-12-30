01cd5072211061fac5da5087d5b83eec79e908e1
ShubhamGomdhare <54259057+ShubhamGomdhare@users.noreply.github.com>
2020-12-30 22:51:13 +0530

    commit

---
+++ app/src/main/java/com/sghost/emotiontracker/FaceDetection.java
@@ -0,0 +1,17 @@
+package com.sghost.emotiontracker;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class FaceDetection extends Application {
+
+    public static final String RESULT_TEXT = "RESULT_TEXT";
+    public static final String RESULT_DIALOG = "RESULT_DIALOGUE";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
